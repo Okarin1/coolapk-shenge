d1bfce51785970d29a8453b56500de7c43b12b98
Okarin1 <Okarin1024@gmail.com>
2023-02-08 02:13:11 +0800

    feat init

---
+++ README.md
@@ -0,0 +1,6 @@
+# 沈哥动态整理
+
+- 基于 [VuePress](https://vuepress.vuejs.org/)
+- 主题 [vuepress-theme-reading](https://github.com/okarin1/vuepress-theme-reading)
+- 自动部署 [vercel](https://vercel.com/)
+
---
+++ example/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: '寓庸笔谈',
+    description: '整理沈哥笔记，共勉之',
+    theme: "reading",
+    themeConfig: {
+      author:'Okarin',
+      authorLink: 'https://okarin.cn',
+      sortMethod:'category'
+    }
+  }
