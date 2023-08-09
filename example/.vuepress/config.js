6980428023ac5af481b25d27a4f7fd6ed886c1db
Okarin <82562407+Okarin1@users.noreply.github.com>
2023-08-09 16:06:15 +0800

    Update config.js

--- example/.vuepress/config.js
+++ example/.vuepress/config.js
@@ -3,8 +3,8 @@ module.exports = {
     description: '整理沈哥笔记，共勉之',
     theme: "reading",
     themeConfig: {
-      author:'获取源文档',
-      authorLink: 'https://github.com/Okarin1/coolapk-shenge/tree/master/example',
+      author:'Github/获取源文档',
+      authorLink: 'https://github.com/Okarin1/coolapk-shenge',
       sortMethod:'category'
     }
   }
